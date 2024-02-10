6ee41d871701ec418d3fb42b57429353aa5031e1
Piotr Sikorski <piotrek.sikorski@hotmail.com>
2024-02-10 16:34:21 +0100

    remove old service

--- src/core/nosql_crud.py
+++ src/core/nosql_crud.py
@@ -3,19 +3,19 @@ from bson.objectid import ObjectId
 
 
 class BaseCRUD:
-    def get(self, db: Collection, id: str):
+    def get(self, db: Collection, id: str) -> dict:
         return db.find_one({"_id": ObjectId(id)})
 
-    def create(self, db: Collection, obj_in: dict):
+    def create(self, db: Collection, obj_in: dict) -> dict:
         id = db.insert_one(obj_in).inserted_id
         return db.find_one({"_id": id})
     
-    def update(self, db: Collection, id: str, obj_in: dict):
+    def update(self, db: Collection, id: str, obj_in: dict) -> dict:
         db.update_one({"_id": ObjectId(id)}, {"$set": obj_in})
         return db.find_one({"_id": ObjectId(id)})
 
-    def delete(self, db: Collection, id: str):
-        db.delete_one({"_id": ObjectId(id)})
+    def delete(self, db: Collection, id: str) -> bool:
+        return (db.delete_one({"_id": ObjectId(id)})).acknowledged
         
-    def get_collection(self, db: Collection):
+    def get_collection(self, db: Collection) -> list:
         return list(db.find())
